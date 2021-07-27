eb3d0cc60aaa5050c9fd1a7f984b6dd7f3455d67
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-07-27 12:46:19 -0300

    Created User entity with the following columns:
    user id (id, int)
    first name (String)
    last name (String)
    email (String)
    password (String)
    photo (String)
    role (Role)
    creation date (Date)
    is deleted (Boolean)
    
    The role column was created using @ManyToOne and @JoinColumn, thus there must exist an entity class named Role.
    The creation date was created using @CreationTimestamp and @Temporal annotations.
    
    The softdelete was implemented with a deleted status column, default value false.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/model/UserModel.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/model/UserModel.java
@@ -1,4 +1,43 @@
 package alkemy.challenge.Challenge.Alkemy.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table (name = "user")
 public class UserModel {
+
+    @Id @Column (name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "1")
+    private int userid;
+
+    @Column (name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column (name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column (name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column (name = "password", nullable = false)
+    private String password;
+
+    @Column (name = "photo")
+    private String photo;
+
+    @ManyToOne (fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;  // Role class must be created or refactor name
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column (name = "creation_date")
+    private Date creationDate;
+
+    @Column (name = "is_deleted", columnDefinition = "default 'false'")
+    private Boolean isDeleted;
+    
 }
