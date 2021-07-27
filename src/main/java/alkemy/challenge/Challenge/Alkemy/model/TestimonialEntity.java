e7f2580d834d5ef9e8d7c1bd8af8624f26f90e61
Vane <mv382038@gmail.com>
2021-07-27 11:09:24 -0300

    I added testimonial entity and repository

---
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/model/TestimonialEntity.java
@@ -0,0 +1,31 @@
+package alkemy.challenge.Challenge.Alkemy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+
+@Entity @Table(name = "testimonials")
+@SQLDelete(sql = "UPDATE testimonials SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+public class TestimonialEntity {
+    @Id
+    private Integer id;
+    @NotNull
+    private String name;
+    private String image;
+    private String content;
+    private Boolean deleted = false;
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creation;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date update;
+}
