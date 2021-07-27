a029b679ec1197a3894cbc1276fb203bf72e6f44
brendacap <bcappadoro@gmail.com>
2021-07-27 12:25:50 -0300

    Role entity and interface created

---
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package alkemy.challenge.Challenge.Alkemy.repository;
+
+import alkemy.challenge.Challenge.Alkemy.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Long, Role> {
+}
