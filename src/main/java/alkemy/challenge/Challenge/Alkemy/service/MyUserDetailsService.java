1467d0f675a5edcd5ef58cd7ae7d76530c50dc72
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-07-28 12:25:49 -0300

    Created UserDetailsModel Class, implementing UserDetails interface. Created a constructor passing Email and Password as arguments.
    In MyUserDetailsService:
    Finished loadUserByUsername method.
    Finished registerUserInDB method, adding exception cases where the user is already in the DB.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/MyUserDetailsService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package alkemy.challenge.Challenge.Alkemy.service;
 
+import alkemy.challenge.Challenge.Alkemy.model.UserDetailsModel;
 import alkemy.challenge.Challenge.Alkemy.model.UserModel;
 import alkemy.challenge.Challenge.Alkemy.repository.UserRepository;
 import alkemy.challenge.Challenge.Alkemy.config.BCryptConfiguration;
@@ -24,17 +25,20 @@ public class MyUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<UserModel> user = userRepository.findByEmail(email);
         user.orElseThrow(() -> new UsernameNotFoundException("Usuario " + email + " no encontrado."));
-        return user.map(
+        return user.map(UserDetailsModel::new).get();
     }
 
-    // Returns message "User ____ has been registered.
+    // Returns message "User ____ has been registered or registration failed.
     public String registerUserInDB(UserModel registeringUser) {
+        String prompt = "El usuario " + registeringUser.getEmail() + " ya se encuentra registrado.";
         try {
             loadUserByUsername(registeringUser.getEmail());
+        } catch (UsernameNotFoundException e){
+            String encryptedPassword = bcryptEnconder.encode(registeringUser.getPassword());
+            registeringUser.setPassword(encryptedPassword);
+            userRepository.save(registeringUser);
+            prompt = "El usuario " + registeringUser.getEmail() + " ha sido registrado.";
         }
-        String encryptedPassword = bcryptEnconder.encode(registeringUser.getPassword());
-        registeringUser.setPassword(encryptedPassword);
-        userRepository.save(registeringUser);
-        return ("El usuario " + registeringUser.getEmail() + " ha sido registrado.");
+        return prompt;
     }
 }
