09c1dd633b2729fd87a6be002de814f9329da464
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-07-27 21:24:43 -0300

    Added BCrypt Configuration.
    Controller:
    Refactored UserServices name to MyUserDetailsService.
    Completed logic of POST.
    
    Services (WIP):
    Inyected bCrypt Encoder, Override loadUserByUsername for searching for email, as emails are the unique ID in Users table. Throws User not found exception.
    registerUserInDB loads by username or else throws exception, encypts password with bCrypt injection and sets as password for the registered user passed as argument. This method returns a confirmation of registration with a string.
    
    Added findByEmail method in User Repository.

--- src/main/java/alkemy/challenge/Challenge/Alkemy/controller/UserRegistrationController.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/controller/UserRegistrationController.java
@@ -1,19 +1,20 @@
 package alkemy.challenge.Challenge.Alkemy.controller;
 
-import alkemy.challenge.Challenge.Alkemy.service.UserServices;
+import alkemy.challenge.Challenge.Alkemy.model.UserModel;
+import alkemy.challenge.Challenge.Alkemy.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserRegistrationController {
 
     @Autowired
-    private UserServices userServices;
+    private MyUserDetailsService myUserDetailsService;
 
     @PostMapping (path = "auth/register")
-    public String registerUser(UserModel ){
-
-
+    public String registerUser(@RequestBody UserModel registeringUser){
+        return myUserDetailsService.registerUserInDB(registeringUser);
     }
 }
--- src/main/java/alkemy/challenge/Challenge/Alkemy/service/MyUserDetailsService.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/service/MyUserDetailsService.java
@@ -1,11 +1,15 @@
 package alkemy.challenge.Challenge.Alkemy.service;
 
+import alkemy.challenge.Challenge.Alkemy.model.UserModel;
 import alkemy.challenge.Challenge.Alkemy.repository.UserRepository;
+import alkemy.challenge.Challenge.Alkemy.config.BCryptConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -13,9 +17,24 @@ public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptConfiguration bcryptEnconder;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<UserModel> user = userRepository.findByEmail(email);
+        user.orElseThrow(() -> new UsernameNotFoundException("Usuario " + email + " no encontrado."));
+        return user.map(
+    }
+
+    // Returns message "User ____ has been registered.
+    public String registerUserInDB(UserModel registeringUser) {
+        try {
+            loadUserByUsername(registeringUser.getEmail());
+        }
+        String encryptedPassword = bcryptEnconder.encode(registeringUser.getPassword());
+        registeringUser.setPassword(encryptedPassword);
+        userRepository.save(registeringUser);
+        return ("El usuario " + registeringUser.getEmail() + " ha sido registrado.");
     }
 }
