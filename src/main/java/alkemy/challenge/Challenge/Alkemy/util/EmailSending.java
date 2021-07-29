701d30967884ed06bdefb14ac8362c04a5e7ec30
jorgebritos <83886610+jorgebritos@users.noreply.github.com>
2021-07-29 19:30:06 -0300

    EmailSending optimizated, Test and Registration Emails Added

--- src/main/java/alkemy/challenge/Challenge/Alkemy/util/EmailSending.java
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/util/EmailSending.java
@@ -13,30 +13,59 @@ import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 
 public class EmailSending {
-	
-public EmailSending (Email receiver) throws IOException {
-	String apiKey = "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";
-    Email from = new Email("britosj30@gmail.com");
-    String subject = "Sending with SendGrid is not Fun";
-    Email to = new Email();
-    to = receiver;
-    Content content = new Content("text/plain", "I'm crying");
-    Mail mail = new Mail(from, subject, to, content);
-    
-	// System.out.println("API Key is " + System.getenv("SENDGRID_API_KEY"));
 
-    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-    Request request = new Request();
-    try {
-      request.setMethod(Method.POST);
-      request.setEndpoint("mail/send");
-      request.setBody(mail.build());
-      Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
+	// Void Constructor
+	public EmailSending() {
+
 	}
-  }
+
+	// Test Email
+
+	public void sending(Email to, String subject, Content content) throws IOException {
+		String apiKey = "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";
+		Email from = new Email("britosj30@gmail.com");
+		Mail mail = new Mail(from, subject, to, content);
+
+		SendGrid sg = new SendGrid(apiKey);
+		Request request = new Request();
+
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println(response.getStatusCode());
+			System.out.println(response.getBody());
+			System.out.println(response.getHeaders());
+		} catch (IOException ex) {
+			throw ex;
+		}
+	}
+
+	// Registration Email
+
+	public void registerEmail(Email to) throws IOException {
+		String apiKey = "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";
+		Email from = new Email("britosj30@gmail.com");
+		String subject = "Email Registration";
+		Content content = new Content("text/plain", "You've registered succesfully");
+
+		Mail mail = new Mail(from, subject, to, content);
+
+		SendGrid sg = new SendGrid(apiKey);
+		Request request = new Request();
+
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println(response.getStatusCode());
+			System.out.println(response.getBody());
+			System.out.println(response.getHeaders());
+		} catch (IOException ex) {
+			throw ex;
+		}
+	}
+
 }
