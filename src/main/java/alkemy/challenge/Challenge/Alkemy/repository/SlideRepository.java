3aacb77a6de756c1d9cc4b0791fb1965574ec52a
brendacap <bcappadoro@gmail.com>
2021-08-02 10:06:37 -0300

    Slide entity and repository created

---
+++ src/main/java/alkemy/challenge/Challenge/Alkemy/repository/SlideRepository.java
@@ -0,0 +1,7 @@
+package alkemy.challenge.Challenge.Alkemy.repository;
+
+import alkemy.challenge.Challenge.Alkemy.model.Slide;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SlideRepository extends JpaRepository<Long, Slide> {
+}
